0ae9dd03505a7814fc47ad11c162d43dd9454f7a
Jisu70 <126372181+Jisu70@users.noreply.github.com>
2023-06-22 03:44:21 +0000

    created category wise and month wise finding query

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// Dependencies 
-const express = require('express') ;
+// Dependencies
+const express = require("express");
 
-const app = express() ;
+const app = express();
 
-const cors = require('cors')
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
-const bodyparser = require('body-parser')
+const bodyparser = require("body-parser");
 
-app.use(bodyparser.urlencoded({extended : true}))
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.use(express.json())
+app.use(express.json());
 
-const mainRouter = require('./route/expences.main') ;
+const mainRouter = require("./route/expences.main");
 
-const database = require('./model/expences.model')
+const database = require("./model/expences.model");
 
-app.use(mainRouter)
+app.use(mainRouter);
 
 // To define the model in dtabase
 database.sequelize
@@ -25,11 +25,8 @@ database.sequelize
   .then()
   .catch((err) => console.log(err));
 
-
-// Starting the server 
-  app.listen(3000, (err) => {
-    if (err) throw err;
-    console.log(" app listen on port 3000 ");
-  });
-  
-
+// Starting the server
+app.listen(3000, (err) => {
+  if (err) throw err;
+  console.log(" app listen on port 3000 ");
+});
--- util/database.js
+++ util/database.js
@@ -1,10 +1,21 @@
 // Dependencies
-const Sequelize = require('sequelize') ; 
+// const Sequelize = require('sequelize') ;
 
-const sequelize = new Sequelize('advanceExpences_tracker', 'root', '1234567890', {
-  dialect : 'mysql',
-  host : 'localhost'
-})
+// const sequelize = new Sequelize('advanceExpences_tracker', 'root', '1234567890', {
+//   dialect : 'mysql',
+//   host : 'localhost'
+// })
 
-module.exports = sequelize
+// module.exports = sequelize
 
+// Dependencies
+// for online database
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("sql12626438", "sql12626438", "FPIdqsSDys", {
+  host: "sql12.freesqldatabase.com",
+  dialect: "mysql",
+  // logging: false, // This will stop the connection message from console
+});
+
+module.exports = sequelize;
